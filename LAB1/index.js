d388c01753b389147ac69299c1c1f5d4087c0b54
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2025-03-02 11:55:26 +0200

    res LAB1

--- LAB1/index.js
+++ LAB1/index.js
@@ -1,42 +1,26 @@
-// Функція для розрахунку параметрів прямокутного трикутника
-type TriangleType =
-  | "leg"
-  | "hypotenuse"
-  | "adjacent angle"
-  | "opposite angle"
-  | "angle";
-
-function triangle(
-  value1: number,
-  type1: TriangleType,
-  value2: number,
-  type2: TriangleType
-): string {
+"use strict";
+function triangle(value1, type1, value2, type2) {
   console.log(`>> triangle(${value1}, "${type1}", ${value2}, "${type2}");`);
 
-  const toRadians = (angle: number) => (Math.PI / 180) * angle;
-  const toDegrees = (radians: number) => (180 / Math.PI) * radians;
-
-  let a: number = 0,
-    b: number = 0,
-    c: number = 0;
-  let alpha: number = 0,
-    beta: number = 0;
-
+  const toRadians = (angle) => (Math.PI / 180) * angle;
+  const toDegrees = (radians) => (180 / Math.PI) * radians;
+  let a = 0,
+    b = 0,
+    c = 0;
+  let alpha = 0,
+    beta = 0;
   if (value1 <= 0 || value2 <= 0) {
     console.log('"Zero or negative input"');
     return "failed";
   }
 
-  // Визначення сторін та кутів залежно від типів аргументів
   const params = new Map([
     [type1, value1],
     [type2, value2],
   ]);
-
   if (params.has("leg") && params.has("hypotenuse")) {
-    a = params.get("leg")!;
-    c = params.get("hypotenuse")!;
+    a = params.get("leg");
+    c = params.get("hypotenuse");
     if (a >= c) {
       console.log(
         "Error: Leg cannot be greater than or equal to the hypotenuse."
@@ -47,20 +31,20 @@ function triangle(
     alpha = toDegrees(Math.asin(a / c));
     beta = 90 - alpha;
   } else if (params.has("leg") && params.has("adjacent angle")) {
-    a = params.get("leg")!;
-    alpha = params.get("adjacent angle")!;
+    a = params.get("leg");
+    alpha = params.get("adjacent angle");
     b = a * Math.tan(toRadians(alpha));
     c = Math.sqrt(a * a + b * b);
     beta = 90 - alpha;
   } else if (params.has("leg") && params.has("opposite angle")) {
-    a = params.get("leg")!;
-    beta = params.get("opposite angle")!;
+    a = params.get("leg");
+    beta = params.get("opposite angle");
     b = a / Math.tan(toRadians(beta));
     c = Math.sqrt(a * a + b * b);
     alpha = 90 - beta;
   } else if (params.has("hypotenuse") && params.has("angle")) {
-    c = params.get("hypotenuse")!;
-    alpha = params.get("angle")!;
+    c = params.get("hypotenuse");
+    alpha = params.get("angle");
     a = c * Math.sin(toRadians(alpha));
     b = c * Math.cos(toRadians(alpha));
     beta = 90 - alpha;
@@ -70,7 +54,6 @@ function triangle(
     );
     return "failed";
   }
-
   console.log(` a = ${a.toFixed(6)}`);
   console.log(` b = ${b.toFixed(12)}`);
   console.log(` c = ${c.toFixed(6)}`);
@@ -80,7 +63,6 @@ function triangle(
   return "success";
 }
 
-// Тестові виклики функції
 triangle(7, "leg", 18, "hypotenuse");
 triangle(60, "opposite angle", 5, "leg");
 triangle(43.13, "angle", -2, "hypotenuse");
